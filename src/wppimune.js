d377eec4ff0a01b07bc11d72bf6b16603553a53a
Mateus726 <Mateusfigueiredo083@gmail.com>
2021-01-10 21:11:00 -0400

    first commit

---
+++ src/wppimune.js
@@ -0,0 +1,44 @@
+const databases = (prefix) => {
+
+return `🔰 Databases de travas 🔰
+
+DB NESCAU V8 DIAMOND: https://enrt.eu/0o2tqQ
+
+𝐃𝐁 𝐕5 𝐁𝐘 𝐆𝐑𝐈𝐍𝐆𝐎 𝐌𝐃𝐙🔥: https://enrt.eu/8aZDGC
+
+DATABASE ELMØ V10: https://enrt.eu/KqBGh
+
+DATABASE DO DAVIZINMAKER: https://enrt.eu/e8AaU1w
+`
+}
+
+exports.databases = databases
+
+const imune = (prefix) => {
+return `🔰 Whatsapp imunes 🔰
+WA ARACHY v3: https://www.mediafire.com/file/4k09vymbh8dzni3/%25E2%259C%25AF%25E2%259C%259E%25EA%2599%25B3%25E1%25AE%259F%25E2%2584%259C%25E1%25AE%259F%25E2%2584%25AD%25E2%2584%258C%25E2%25B2%25AE_%25E1%259C%25A0%25E2%259D%25B8%25EA%2599%25B3%25E2%259C%259E%25E2%259C%25AF.apk/file
+
+MAXYN OF SATANS:: httphttps://mega.nz/file/Oo1CFB5Q#dtIYBG2kqYBm0RgBjHddOHs-Mym_mJ_k-L8DYMiU5I0
+
+MINATO v8: https://suaurl.com/e57887
+
+TERREMOTO v22: https://suaurl.com/210d49
+
+GAARA v5: https://suaurl.com/dde464
+
+IMORTAL v19: https://suaurl.com/225875
+
+Souza Wa v5: https://suaurl.com/8dbb5e
+
+DIURNO v9: http://www.mrtutors.com.br/IDKFr
+
+FLEX v7: https://suaurl.com/b18e0e
+
+PESADELO v7: https://suaurl.com/ddf8a6
+
+FÊNIX v14: https://suaurl.com/145ff5
+
+FROST v17: https://suaurl.com/2eb5cc`
+}
+
+exports.imune = imune
